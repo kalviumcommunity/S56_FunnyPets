b023cd7205fac736d1ca87c168d1254565abd07d
Janhavi003 <janhavi.hivarekar@kalvium.community>
2024-02-28 10:09:19 +0530

    created the landing page using react

---
+++ server/server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const connection = require('./config/db');
+const route = require('./route');
+require("dotenv").config()
+const app = express();
+const port = 3000; 
+
+app.get('/ping', (req, res) => {
+  res.status(200).send('Pong!');
+});
+app.use(route)
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${4}`);
+});
+
+
+const connectToDB = async () => {
+  try {
+    await mongoose.connect(connection.mongoURI)
+    console.log('📦 connected to mongoDB');
+  } catch (err) {
+    console.error('❌ error connecting to mongoDB:', err.message);
+  }
+};
+
+const disconnectFromDB = async () => {
+  try {
+    await mongoose.disconnect()
+    console.log('📦 disconnected from mongoDB');
+  } catch (err) {
+    console.error('❌ error disconnecting from mongoDB:', err.message);
+  }
+};
+
+module.exports = {
+  connectToDB,
+  disconnectFromDB,
+  mongooseConnection: mongoose.connection,
+};
