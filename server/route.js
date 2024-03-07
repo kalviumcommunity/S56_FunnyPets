87a2f75334cf8efd8e0abadf649bc580e8f2d31c
Janhavi003 <janhavi.hivarekar@kalvium.community>
2024-03-07 11:45:44 +0530

    made some changes

--- server/route.js
+++ server/route.js
@@ -5,11 +5,10 @@ router.use(express.json());
 router.get('/get',(req,res)=>{
     res.send("It is a get request")
 })
-router.post("/addform", async(req,res)=> {
+router.post("/addform", (req,res)=> {
     try {
       console.log(req.body)
       const user = FunnyPet.create(req.body);
-      await user.save();
       res.send(user);
     } catch (error) {
       res.status(500).send(error);
